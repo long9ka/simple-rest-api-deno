c33c0691c4349c2122c8096af6561a4ce1267833
danh phi long <>
2020-07-20 00:18:47 +0700

    update routes

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,12 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 
+// routes
+import users from "./routes/api/users.ts";
+
 const app = new Application();
 
-app.use((ctx) => {
-  ctx.response.body = "Hello World!";
-});
+// api
+app.use(users.routes(), users.allowedMethods());
 
 console.log("server running...");
 await app.listen({ port: 8000 });
